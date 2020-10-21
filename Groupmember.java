0287ec091598661ee025e3015f0d6dec80d3b927
Likhitha19-hub <72901273+Likhitha19-hub@users.noreply.github.com>
2020-10-21 12:01:38 +0530

    Add files via upload

---
+++ Groupmember.java
@@ -0,0 +1,8 @@
+class Groupmember{
+String name;
+String branch;
+String Collegename;
+void details(){
+System.out.println( name+"\n "+ branch+"\n "+ Collegename "\n");
+}
+}
---
+++ Groupmemberprofile.java
@@ -0,0 +1,36 @@
+class Groupmemberprofile{
+public static void main(String[] args) {
+Groupmember first=new Groupmember();
+first.name ="Likhitha K.P";
+first.branch ="E&C";
+first.Collegename ="AIT,Chikkamagalur";
+first.details();
+
+Groupmember second=new Groupmember();
+second.name ="Bhavya";
+second.branch ="E&C";
+second.Collegename ="APS,Bangalore";
+second.details();
+
+Groupmember third =new Groupmember();
+third.name ="MAMATHA";
+third.branch ="E&C";
+third.Collegename ="BGMIT,Mudhol";
+third.details();
+
+Groupmember fourth=new Groupmember();
+fourth.name ="Mallikarjun";
+fourth.branch ="Civil";
+fourth.Collegename ="AIT,Chikkamagalur";
+fourth.details();
+
+Groupmember fifth=new Groupmember();
+fifth.name ="Kaleel";
+fifth.branch ="cS";
+fifth.Collegename ="AIT,Chikkamagalur";
+fifth.details();
+
+}
+}
+
+
